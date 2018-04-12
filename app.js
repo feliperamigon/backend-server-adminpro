18a7819950cf9f75033ffe590722ea7c7dce775b
Felipe Ramirez <framirez.mill@gmail.com>
2018-04-12 09:17:42 -0500

    JWT and Services done

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 // Requires ( Importacion de librerias tercerizadas o personalizadas)
 var express = require('express');
 var mongoose = require('mongoose'); // Libreria para manejo de MongoDB
-
+var bodyParser = require('body-parser'); // Formatear respuestas POST a  Objectos de JS
 
 
 
 // Inicializar variables
 var app = express(); // Definir servidor express
+
+// Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Conexión a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
@@ -15,15 +20,15 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 });
 
 
-// Rutas
-app.get('/', ( req, res, next ) => {
+//Importar Rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    })
-
-});
+// Rutas (Middleware)
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 
 // Escuchar peticiones
